229ad1fbf357f902d0b264f67fef06a657424320
Leo Developer <leodeveloper@protonmail.com>
2024-11-11 15:36:47 +0100

    update hook

--- src/lib/hook-js.ts
+++ src/lib/hook-js.ts
@@ -1,18 +1,25 @@
+// this is the best I can do currently
+// there is still a way to detect this with the following method:
+//   Object.setPrototypeOf(target, Object.create(target));
+// not hooked functions throw a TypeError due to cyclic prototype,
+// but it works just fine on a hooked function
 export function hookFunction(
 	parent: { [key: string]: any },
 	name: string,
 	fn: Function,
 ) {
-	parent[name] = new Proxy(parent[name], {
+	const original = parent[name];
+	parent[name] = new Proxy(original, {
 		apply(target, thisArg, argArray) {
 			return fn.apply(thisArg, argArray);
 		},
 		get(target, p, receiver) {
 			const method = Reflect.get(target, p, receiver);
-			if (p === "toString") {
-				Object.defineProperty(method, "toString", {
-					value: String.prototype.toString.bind(String.toString),
-					enumerable: false,
+			if (typeof method === "function") {
+				return new Proxy(method, {
+					apply(target, thisArg, argArray) {
+						return method.apply(original, argArray);
+					},
 				});
 			}
 			return method;
